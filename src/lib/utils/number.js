1452d6d77d9ecbb1c707c0afbdf795cfdec112b9
harmen.dev <hello@harmen.dev>
2022-09-26 20:26:44 +0200

    Add number util

---
+++ src/lib/utils/number.js
@@ -0,0 +1,4 @@
+export default function number(value, decimals = 2, method = 'floor') {
+    const operator = Number.parseInt(`1${new Array(decimals).fill(0).join().replace(',', '')}`, 10);
+    return (Math[method](value * operator) / operator).toFixed(decimals);
+}
