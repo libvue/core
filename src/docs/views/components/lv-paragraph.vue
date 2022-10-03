f1d9e62e10d1c29f99afa4dff3ca97012763a7c8
harmen.dev <hello@harmen.dev>
2022-10-03 21:10:20 +0200

    Fix paragraph docs

--- src/docs/views/components/lv-paragraph.vue
+++ src/docs/views/components/lv-paragraph.vue
@@ -1,3 +1,38 @@
 <template>
-    <lv-group> paragraph </lv-group>
+    <lv-heading :level="3" v-space-after="0.5">Paragraph</lv-heading>
+    <lv-heading sub :level="6" v-space-after="1">A paragraph always starts on a new line, and is usually a block of text.</lv-heading>
+
+    <lv-card v-space-after="1">
+        <lv-paragraph>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </lv-paragraph>
+        <lv-paragraph align="center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </lv-paragraph>
+        <lv-paragraph align="right">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </lv-paragraph>
+    </lv-card>
+    <lv-code v-space-after="1" lang="html" :code="codeParagraph" />
 </template>
+
+<script>
+const codeParagraph = `
+<lv-paragraph>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+</lv-paragraph>
+<lv-paragraph align="center">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+</lv-paragraph>
+<lv-paragraph align="right">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+</lv-paragraph>
+`.trim();
+export default {
+    data() {
+        return {
+            codeParagraph,
+        };
+    },
+};
+</script>
--- src/lib/components/LvParagraph/LvParagraph.vue
+++ src/lib/components/LvParagraph/LvParagraph.vue
@@ -1,11 +1,26 @@
 <template>
-    <p class="lv-paragraph">
+    <p class="lv-paragraph" :class="classObject">
         <slot></slot>
     </p>
 </template>
 
 <script>
-export default {};
+export default {
+    props: {
+        align: {
+            type: String,
+            default: 'left',
+            validator: (val) => ['left', 'center', 'right', 'justify'].includes(val)
+        }
+    },
+    computed: {
+        classObject() {
+            return {
+                [`lv-paragraph--align-${this.align}`]: !!this.align,
+            }
+        }
+    }
+};
 </script>
 
 <style lang="scss">
@@ -13,5 +28,21 @@ export default {};
 .lv-paragraph {
     margin: 0;
     font-size: $font-size;
+
+    &--align-left {
+        text-align: left;
+    }
+
+    &--align-center {
+        text-align: center;
+    }
+
+    &--align-right {
+        text-align: right;
+    }
+
+    &--align-justify {
+        text-align: justify;
+    }
 }
 </style>
