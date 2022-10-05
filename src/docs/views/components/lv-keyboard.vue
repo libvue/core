4d277ba3f376d76638195a6f5b3df3b3d26b9d66
harmen.dev <hello@harmen.dev>
2022-10-05 19:50:00 +0200

    Improve docs

--- src/docs/views/components/lv-keyboard.vue
+++ src/docs/views/components/lv-keyboard.vue
@@ -2,7 +2,21 @@
     <lv-heading :level="3" v-space-after="0.5">Keyboard</lv-heading>
     <lv-heading sub :level="6" v-space-after="1">The Keyboard Input element</lv-heading>
 
-    <lv-card>
+    <lv-card v-space-after="1">
         Press <lv-keyboard>CTRL</lv-keyboard> and <lv-keyboard>V</lv-keyboard> to paste your selection.
     </lv-card>
+
+    <lv-code lang="html" :code="code"/>
 </template>
+
+<script>
+const code = `Press <lv-keyboard>CTRL</lv-keyboard> and <lv-keyboard>V</lv-keyboard> to paste your selection.`;
+
+export default {
+    data() {
+        return {
+            code,
+        }
+    }
+}
+</script>
