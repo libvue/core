a00eca918c05fcfac632884e7ce2d5e832d8aed8
Harmen de Vries <harmen@made.frl>
2022-10-07 08:03:01 +0200

    Let v-badge use a scss file for styling

--- src/docs/views/utilities/v-badge.vue
+++ src/docs/views/utilities/v-badge.vue
@@ -2,7 +2,7 @@
     <lv-heading :level="3" v-space-after="0.5">v-badge</lv-heading>
     <lv-heading sub :level="6" v-space-after="1">Badge everything!</lv-heading>
     <lv-paragraph v-space-after="1">
-        We we're tired of adding a badge in every component. Now you can badge everything with a simple directive
+        We're tired of adding a badge in every component. Now you can badge everything with a simple directive
     </lv-paragraph>
     <lv-card v-badge="';)'" v-space-after="1">
         <lv-button label="Im a button" v-badge="'badge!'"/>
--- src/lib/directives/badge.js
+++ src/lib/directives/badge.js
@@ -1,3 +1,5 @@
+import '../scss/components/lv-badge.scss';
+
 const handler = (el, binding) => {
     // Check if lv-badge already exists inside el
     const foundBadge = el.querySelector(':scope > .lv-badge');
@@ -15,16 +17,6 @@ const handler = (el, binding) => {
     badge.innerText = binding.value;
     // Add Class (just for understanding)
     badge.classList.add('lv-badge');
-    // Add styling
-    badge.style.position = 'absolute';
-    badge.style.backgroundColor = 'red';
-    badge.style.color = 'white';
-    badge.style.top = '-0.625rem';
-    badge.style.right = '0';
-    badge.style.transform = 'translateX(50%)';
-    badge.style.fontSize = '0.625rem';
-    badge.style.padding = '0.25rem 0.5rem';
-    badge.style.borderRadius = '0.625rem';
     // Add it to the element
     el.appendChild(badge);
 };
