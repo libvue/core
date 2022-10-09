c0957dcc07893d2a461c6f104a32e524b701b585
harmen.dev <hello@harmen.dev>
2022-10-09 21:42:59 +0200

    Improve select

--- src/docs/views/components/lv-select.vue
+++ src/docs/views/components/lv-select.vue
@@ -4,17 +4,34 @@
     <lv-card v-space-after="1">
         <lv-select v-model="model" :options="options" />
     </lv-card>
-    <lv-code :code="code" lang="html"/>
+    <lv-code :code="code" v-space-after="1" lang="html"/>
+    <lv-code :code="codeScript" lang="js"/>
 </template>
 
 <script>
 const code = `
 <lv-select v-model="model" :options="options" />
 `.trim();
+const codeScript = `
+export default {
+    data() {
+        return {
+            model: 1,
+            options: [
+                { label: "Option 1", value: 1, image: 'https://picsum.photos/50/50' },
+                { label: "Option 2", value: 2, image: 'https://picsum.photos/50/50?1' },
+                { label: "Option 3", value: 3, image: 'https://picsum.photos/50/50?2', disabled: true },
+                { label: "Option 4", value: 4, image: 'https://picsum.photos/50/50?3' }
+            ]
+        }
+    }
+}
+`.trim();
 export default {
     data() {
         return {
             code,
+            codeScript,
             model: 1,
             options: [
                 { label: "Option 1", value: 1, image: 'https://picsum.photos/50/50' },
