1ed42738ecadf604f423e5f61b111df9a521abb1
Harmen de Vries <harmen@made.frl>
2022-10-24 17:08:34 +0200

    Improve hero

--- src/docs/views/home.vue
+++ src/docs/views/home.vue
@@ -1,12 +1,30 @@
 <template>
     <div class="splash">
 
-        <lv-hero title="Libvue" description="An extendable collection of Vue components to quickly scaffold interfaces.">
+        <lv-hero title="Libvue" description="An extendable collection of Vue components to quickly scaffold interfaces." v-space-after="4">
             <template #buttons>
                 <lv-button color="primary" icon="book-open" @click="$router.push('install')">Documentation</lv-button>
                 <lv-button color-type="outline" icon="github" @click="goToGithub">Github</lv-button>
             </template>
         </lv-hero>
+
+        <lv-divider v-space-after="4"></lv-divider>
+
+        <lv-group direction="column" style="width: 100%;" gap="2rem">
+            <lv-card>
+                <lv-heading v-space-after="1" level="6">Core</lv-heading>
+                <lv-code code="> npm install --save @libvue/core" lang="shell" />
+            </lv-card>
+            <lv-card>
+                <lv-heading v-space-after="1" level="6">Extension: Dashboard</lv-heading>
+                <lv-code code="> npm install --save @libvue/extension-dashboard" lang="shell" />
+            </lv-card>
+            <lv-card>
+                <lv-heading v-space-after="1" level="6">Extension: Home Automation</lv-heading>
+                <lv-code code="> npm install --save @libvue/extension-home-automation" lang="shell" />
+            </lv-card>
+        </lv-group>
+
     </div>
 </template>
 
@@ -25,12 +43,10 @@ export default {
 
 .splash {
     display: flex;
-    align-items: center;
-    justify-content: center;
+    align-items: flex-start;
     flex-direction: column;
     height: 100%;
     margin: auto;
-    padding-top: 150px;
-
+    padding-top: 100px;
 }
 </style>
