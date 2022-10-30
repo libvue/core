a672f9ac83b14c842ae8e13d8b6dc671f6e6309a
harmen.dev <hello@harmen.dev>
2022-10-30 21:40:13 +0100

    Update textarea

--- src/docs/views/documentation/components/lv-textarea.vue
+++ src/docs/views/documentation/components/lv-textarea.vue
@@ -1,11 +1,19 @@
 <template>
-    <lv-heading v-space-after="0.5" :level="3">LvTextarea</lv-heading>
-    <lv-heading v-space-after="2" sub :level="6">Component Description</lv-heading>
+    <lv-heading v-space-after="0.5" :level="3">Textarea</lv-heading>
+    <lv-heading v-space-after="2" sub :level="6">Area to write long texts for within a form</lv-heading>
     <lv-card v-space-after="1">
         <lv-textarea/>
     </lv-card>
+    <lv-code :code="code" lang="html"/>
 </template>
 
 <script>
-export default {}
+const code = `<lv-textarea/>`.trim();
+export default {
+    data() {
+        return {
+            code,
+        }
+    }
+}
 </script>
--- src/lib/components/LvTextarea/LvTextarea.vue
+++ src/lib/components/LvTextarea/LvTextarea.vue
@@ -1,13 +1,28 @@
 <template>
-    <div class="lv-textarea">
-        Hello World
-    </div>
+    <textarea class="lv-textarea" :placeholder="placeholder"></textarea>
 </template>
 
 <script>
-export default {};
+export default {
+    props: {
+        placeholder: {
+            type: String,
+            default: 'Enter your text here'
+        }
+    }
+};
 </script>
 
 <style lang="scss">
-.lv-textarea {}
+.lv-textarea {
+    width: 100%;
+    background-color: var(--background-color);
+    color: var(--text-color);
+    padding: var(--padding);
+    margin: 0;
+    resize: vertical;
+    border-radius: var(--border-radius);
+    font-family: var(--font-family);
+    border: 1px solid var(--border-color);
+}
 </style>
