282b423c0c1c5af7240a4af7e5829400d78854f7
harmen.dev <hello@harmen.dev>
2022-11-07 21:47:00 +0100

    Add Skeleton component

---
+++ src/docs/views/documentation/components/lv-skeleton.vue
@@ -0,0 +1,37 @@
+<template>
+    <lv-heading v-space-after="0.5" :level="3">Skeleton</lv-heading>
+    <lv-heading v-space-after="2" sub :level="6">Reserve space for upcoming data!</lv-heading>
+    <lv-card v-space-after="1">
+        <lv-skeleton/>
+    </lv-card>
+    <lv-code v-space-after="1" lang="html" :code="codeDefault"/>
+    <lv-card v-space-after="1">
+        <lv-skeleton width="200px" height="100px"/>
+    </lv-card>
+    <lv-code v-space-after="1" lang="html" :code="codeDimensions"/>
+    <lv-card v-space-after="1">
+        <lv-group>
+            <lv-skeleton width="50px" height="50px" rounded/>
+            <lv-skeleton width="50px" height="50px" squared/>
+        </lv-group>
+    </lv-card>
+    <lv-code v-space-after="1" lang="html" :code="codeShapes"/>
+</template>
+
+<script>
+const codeDefault = `<lv-skeleton/>`.trim();
+const codeDimensions = `<lv-skeleton width="200px" height="100px"/>`.trim();
+const codeShapes = `
+<lv-skeleton width="50px" height="50px" rounded/>
+<lv-skeleton width="50px" height="50px" squared/>
+`.trim();
+export default {
+    data() {
+        return {
+            codeDefault,
+            codeDimensions,
+            codeShapes
+        }
+    }
+}
+</script>
---
+++ src/lib/components/LvSkeleton/LvSkeleton.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="lv-skeleton" :class="classObject"></div>
+</template>
+
+<script>
+export default {
+    props: {
+        width: {
+            type: String,
+            default: '100px'
+        },
+        height: {
+            type: String,
+            default: 'auto',
+        },
+        squared: {
+            type: Boolean,
+            default: false,
+        },
+        rounded: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    computed: {
+        classObject() {
+            return {
+                'lv-skeleton--squared': !!this.squared,
+                'lv-skeleton--rounded': !!this.rounded,
+            }
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+.lv-skeleton {
+    display: inline-flex;
+    min-height: 1rem;
+    background-image: linear-gradient(270deg,var(--skeleton-tint1),var(--skeleton-tint2),var(--skeleton-tint2),var(--skeleton-tint1));
+    background-size: 400% 100%;
+    animation: background 8s ease-in-out infinite;
+    border-radius: var(--border-radius);
+    width: v-bind(width);
+    height: v-bind(height);
+    &--rounded {
+        border-radius: 100%;
+    }
+    &--squared {
+        border-radius: 0;
+    }
+}
+@keyframes background {
+    0% {
+        background-position: 200% 0;
+    }
+    100% {
+        background-position: -200% 0;
+    }
+}
+</style>
