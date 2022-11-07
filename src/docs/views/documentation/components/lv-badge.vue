67a9875e359f19ab16c15f3c3e6c4cec073a9183
harmen.dev <hello@harmen.dev>
2022-11-07 21:42:37 +0100

    Add badge docs

---
+++ src/docs/views/documentation/components/lv-badge.vue
@@ -0,0 +1,43 @@
+<template>
+    <lv-heading v-space-after="0.5" :level="3">Badge</lv-heading>
+    <lv-heading v-space-after="2" sub :level="6">A simple badge component</lv-heading>
+    <lv-heading v-space-after="1" :level="6">Standalone</lv-heading>
+    <lv-card v-space-after="1">
+        <lv-group>
+            <lv-badge text="hello" />
+            <lv-badge text="hello" color="solid-warning" />
+            <lv-badge text="hello" color="solid-dimmed-success" />
+            <lv-badge text="hello" color="outline-info" />
+        </lv-group>
+    </lv-card>
+    <lv-code v-space-after="1" lang="html" :code="codeDefault" />
+    <lv-heading v-space-after="1" :level="6">Slotted</lv-heading>
+    <lv-card v-space-after="1">
+        <lv-badge text="hello">
+            <lv-button label="Im a button" />
+        </lv-badge>
+    </lv-card>
+    <lv-code v-space-after="1" lang="html" :code="codeSlotted" />
+</template>
+
+<script>
+const codeDefault = `
+<lv-badge text="hello" />
+<lv-badge text="hello" color="solid-warning" />
+<lv-badge text="hello" color="solid-dimmed-success" />
+`.trim();
+const codeSlotted = `
+<lv-badge text="hello">
+    <lv-button label="Im a button"/>
+</lv-badge>
+`.trim();
+
+export default {
+    data() {
+        return {
+            codeDefault,
+            codeSlotted,
+        };
+    },
+};
+</script>
