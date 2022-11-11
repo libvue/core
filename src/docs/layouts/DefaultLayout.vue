f2f1550f859676dcbf229d143d4c2253b61db7da
harmen.dev <hello@harmen.dev>
2022-11-11 21:22:35 +0100

    prepare file-input component

--- src/docs/layouts/DefaultLayout.vue
+++ src/docs/layouts/DefaultLayout.vue
@@ -114,6 +114,11 @@
                     <lv-vertical-nav-item to="/docs/lv-input" label="Input" />
                     <lv-vertical-nav-item to="/docs/lv-checkbox" label="Checkbox" />
                     <lv-vertical-nav-item label="Radio" to="/docs/lv-radio" />
+                    <lv-vertical-nav-item to="/docs/lv-file-input" disabled>
+                        <lv-group gap=".25rem">
+                            File Input <lv-badge text="Planned" size="tiny" color="solid-dimmed-primary" />
+                        </lv-group>
+                    </lv-vertical-nav-item>
                     <lv-vertical-nav-item label="Slider" to="/docs/lv-slider" />
                     <lv-vertical-nav-item label="Range Slider" to="/docs/lv-range-slider" />
                     <lv-vertical-nav-item label="Switch" to="/docs/lv-switch" />
---
+++ src/docs/views/documentation/components/lv-file-input.vue
@@ -0,0 +1,19 @@
+<template>
+    <lv-heading v-space-after="0.5" :level="3">LvFileInput</lv-heading>
+    <lv-heading v-space-after="2" sub :level="6">Component Description</lv-heading>
+    <lv-card v-space-after="1">
+        <lv-file-input/>
+    </lv-card>
+    <lv-code lang="html" :code="code"/>
+</template>
+
+<script>
+const code = ``.trim();
+export default {
+    data() {
+        return {
+            code,
+        }
+    }
+}
+</script>
