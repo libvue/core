cc0036fca9f362ed60f3525de50c60d7c52fd70b
harmen.dev <hello@harmen.dev>
2022-11-13 22:46:09 +0100

    Change base and add .nojekyll

--- docs/LvAdvertApp.cc8ff2f5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as o}from"./index.608cace3.js";const s={},_={class:"lv-advert-app"};function c(r,n,p,a,d,l){return t(),o("div",_," Hello World ")}const i=e(s,[["render",c]]);export{i as default};
