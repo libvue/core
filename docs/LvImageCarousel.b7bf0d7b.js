deb121b10b7187a4f32546c2affa1a31ed1f53c9
harmen.dev <hello@harmen.dev>
2022-11-22 22:58:07 +0100

    add new docs

---
+++ docs/LvImageCarousel.b7bf0d7b.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as s}from"./index.c47e6032.js";const t={},a={class:"lv-image-carousel"};function _(c,r,n,l,p,u){return s(),o("div",a," Hello World ")}const i=e(t,[["render",_]]);export{i as default};
---
+++ docs/LvToggleGroup.1ae1caa5.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,r as s,o as r}from"./index.c47e6032.js";const _={},n={class:"lv-toggle-group"};function c(e,a,l,p,u,d){return r(),t("div",n,[s(e.$slots,"default")])}const f=o(_,[["render",c]]);export{f as default};
