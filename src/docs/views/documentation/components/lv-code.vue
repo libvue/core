022d28cd3b6cfb1ac83f10249008d29dfdb427c1
harmen.dev <hello@harmen.dev>
2022-11-24 22:30:01 +0100

    Improve styling of code component

--- src/docs/views/documentation/components/lv-code.vue
+++ src/docs/views/documentation/components/lv-code.vue
@@ -4,20 +4,20 @@
 
     <lv-heading v-space-after="1" :level="6">Default</lv-heading>
     <lv-card v-space-after="1">
-        <lv-code code="console.log('Sup!')" lang="javascript" />
+        <lv-code code="Hello World" lang="html" />
     </lv-card>
     <lv-code v-space-after="1" :code="code" lang="html" />
 
-    <lv-heading v-space-after="1" :level="6">With title</lv-heading>
+    <lv-heading v-space-after="1" :level="6">With Files</lv-heading>
     <lv-card v-space-after="1">
-        <lv-code code="console.log('Sup!')" lang="javascript" title="app.js" />
+        <lv-code :files="files" :active="activeFile" @change-file="(v) => activeFile = v"/>
     </lv-card>
     <lv-code v-space-after="1" :code="codeWithTitle" lang="html" />
     <component-details component="LvCode"></component-details>
 </template>
 
 <script>
-const code = `<lv-code code="console.log('Sup!')" lang="javascript" />`.trim();
+const code = `<lv-code code="Hello World" lang="html" />`.trim();
 const codeWithTitle = `<lv-code code="console.log('Sup!')" lang="javascript" />`.trim();
 
 export default {
@@ -25,6 +25,11 @@ export default {
         return {
             code,
             codeWithTitle,
+            activeFile: 'app',
+            files: [
+                { id: 'app', filename: 'app.js', lang: 'js', code: 'app.start()' },
+                { id: 'css', filename: 'app.css', lang: 'css', code: '* { color: red }' },
+            ]
         };
     },
 };
