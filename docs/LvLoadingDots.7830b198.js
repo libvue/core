d163318b2da3f4e2de3c8b8a018ed618a030fa55
Harmen de Vries <harmen@made.frl>
2022-12-09 12:41:57 +0100

    Add docs for version 0.0.31

--- docs/LvLoadingDots.7830b198.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as t,o as _,a as o}from"./index.3f640ef4.js";const e={},d={class:"lv-loading-dots"},n=o("div",{class:"lv-loading-dots__dot"},null,-1),a=o("div",{class:"lv-loading-dots__dot"},null,-1),c=o("div",{class:"lv-loading-dots__dot"},null,-1),l=[n,a,c];function i(r,v,p,u,g,f){return _(),t("div",d,l)}const m=s(e,[["render",i]]);export{m as default};
