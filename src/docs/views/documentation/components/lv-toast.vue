a01a35417b9093be866c1c6e495e6654ef1b4627
Harmen de Vries <harmen@made.frl>
2022-12-14 13:44:04 +0100

    Improve toast

--- src/docs/views/documentation/components/lv-toast.vue
+++ src/docs/views/documentation/components/lv-toast.vue
@@ -2,53 +2,111 @@
     <lv-heading v-space-after="0.5" :level="3">Toast</lv-heading>
     <lv-heading v-space-after="2" sub :level="6">Important messages at your service</lv-heading>
 
-    <lv-heading v-space-after="1" :level="6">Component</lv-heading>
-    <lv-card v-space-after="1">
-        <lv-toast description="Monday, October 24, 2022 at 9:26 PM" title="Your work is saved" />
-    </lv-card>
-    <lv-code v-space-after="1" lang="html" :code="code" />
-
-    <lv-heading v-space-after="1" :level="6">Programmatic</lv-heading>
-    <lv-card v-space-after="1">
-        <lv-button label="Add toast programmatically" @click="addToast"/>
-        <lv-toasts :max="3" :decay="3000" />
-    </lv-card>
-    <lv-code v-space-after="1" lang="html" :code="codeProgram" />
-    <lv-code v-space-after="1" lang="javascript" :code="codeAddToast" />
-    <component-details component="LvToast"></component-details>
+    <lv-tabs :tabs="tabs" :active="activeTab" @change-tab="(v) => (activeTab = v)">
+        <template #toast>
+            <lv-card v-space-after="1">
+                <lv-toast
+                    v-space-after="1"
+                    icon="save"
+                    description="Monday, October 24, 2022 at 9:26 PM"
+                    title="Your work is saved"
+                    button="Close"
+                    @click-button="onClickButton"
+                />
+                <lv-toast icon="save" title="Your work is saved" button="Close" @click-button="onClickButton" />
+            </lv-card>
+            <lv-code v-space-after="1" lang="html" :code="code" />
+            <component-details component="LvToast"></component-details>
+        </template>
+        <template #toasts>
+            <lv-card v-space-after="1">
+                <lv-toasts :max="3" :decay="1000" />
+                <lv-button color="solid-dimmed-primary" label="Add toast" @click="addToast" />
+            </lv-card>
+            <lv-code
+                v-space-after="1"
+                :files="toastFiles"
+                :active="activeToastFile"
+                @change-file="(v) => (activeToastFile = v)"
+            />
+            <component-details component="LvToasts"></component-details>
+        </template>
+    </lv-tabs>
 </template>
 
 <script>
 const code = `
-<lv-toast description="Monday, October 24, 2022 at 9:26 PM" title="Your work is saved" />
+<lv-toast
+    v-space-after="1"
+    icon="save"
+    description="Monday, October 24, 2022 at 9:26 PM"
+    title="Your work is saved"
+    button="Close"
+    @click-button="onClickButton"
+/>
+<lv-toast
+    icon="save"
+    title="Your work is saved"
+    button="Close"
+    @click-button="onClickButton"
+/>
 `.trim();
-const codeProgram = `
-<lv-button label="Add toast programmatically" @click="addToast"/>
-<lv-toasts :max="3" :decay="3000" />
+const codeTemplate = `
+<lv-toasts :max="3" :decay="1000" />
+<lv-button color="solid-dimmed-primary" label="Add toast" @click="addToast" />
 `.trim();
-const codeAddToast = `
+const codeScript = `
 addToast() {
     this.libvue.addToast({
-        title: 'Howdy!',
-        description: 'This is a toast'
-    })
-}
+        title: 'Ut enim ad minima veniam, quis nostrum exercitationem',
+        description: 'Quis autem vel eum iure reprehenderit',
+        button: 'Do Something',
+        icon: 'save',
+        onClick: () => {
+            console.log('Clicking toast');
+        },
+        onClickButton: () => {
+            console.log('Clicking button');
+        },
+    });
+},
 `.trim();
 export default {
     data() {
         return {
             code,
-            codeProgram,
-            codeAddToast
-        }
+            codeTemplate,
+            codeScript,
+            activeToastFile: 'template',
+            toastFiles: [
+                { id: 'template', filename: 'template', code: codeTemplate, lang: 'html' },
+                { id: 'script', filename: 'script', code: codeScript, lang: 'js' },
+            ],
+            activeTab: 'toasts',
+            tabs: [
+                { id: 'toast', title: 'Toast', icon: 'scaling' },
+                { id: 'toasts', title: 'Toasts', icon: 'palette' },
+            ],
+        };
     },
     methods: {
         addToast() {
             this.libvue.addToast({
-                title: 'Howdy!',
-                description: 'This is a toast',
-            })
-        }
-    }
-}
+                title: 'Ut enim ad minima veniam, quis nostrum exercitationem',
+                description: 'Quis autem vel eum iure reprehenderit',
+                button: 'Do Something',
+                icon: 'save',
+                onClick: () => {
+                    console.log('Clicking toast');
+                },
+                onClickButton: () => {
+                    console.log('Clicking button');
+                },
+            });
+        },
+        onClickButton() {
+            console.log('Clicked Button');
+        },
+    },
+};
 </script>
