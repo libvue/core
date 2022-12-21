46a80d55c57bf614d612b93c96dd3871611032b2
Harmen de Vries <harmen@made.frl>
2022-12-21 14:58:56 +0100

    Add docs for version 0.0.46

---
+++ docs/lv-toggle-group.7cd3e7cd.js
@@ -0,0 +1,26 @@
+import{_ as A,o as i,c as U,w as a,b as p,d as n,e,F as x,f as v,g as s,h as B}from"./index.33814177.js";const C=`
+<lv-flex>
+    <lv-toggle-group>
+        <lv-toggle v-model="pressedAlignLeft">
+            <lv-icon name="align-left"/>
+        </lv-toggle>
+        <lv-toggle v-model="pressedAlignCenter">
+            <lv-icon name="align-center"/>
+        </lv-toggle>
+        <lv-toggle v-model="pressedAlignRight">
+            <lv-icon name="align-right"/>
+        </lv-toggle>
+    </lv-toggle-group>
+    <lv-toggle-group>
+        <lv-toggle v-model="pressedItalic">
+            <lv-icon name="italic"/>
+        </lv-toggle>
+        <lv-toggle v-model="pressedBold">
+            <lv-icon name="bold"/>
+        </lv-toggle>
+        <lv-toggle v-model="pressedUnderline">
+            <lv-icon name="link"/>
+        </lv-toggle>
+    </lv-toggle-group>
+</lv-flex>
+`.trim(),b={data(){return{code:C,pressedItalic:!1,pressedAlign:!1,pressedAlignLeft:!1,pressedAlignCenter:!1,pressedAlignRight:!1,pressedBold:!1,pressedUnderline:!1}}},k=v("Toggle Group"),L=v("A group of toggle buttons");function I(R,o,T,h,l,w){const m=s("lv-heading"),d=s("lv-icon"),g=s("lv-toggle"),u=s("lv-toggle-group",!0),c=s("lv-flex"),_=s("lv-card"),f=s("lv-code"),V=s("component-details"),r=B("space-after");return i(),U(x,null,[a((i(),p(m,{level:3},{default:n(()=>[k]),_:1})),[[r,.5]]),a((i(),p(m,{sub:"",level:6},{default:n(()=>[L]),_:1})),[[r,2]]),a((i(),p(_,null,{default:n(()=>[e(c,null,{default:n(()=>[e(u,null,{default:n(()=>[e(g,{modelValue:l.pressedAlignLeft,"onUpdate:modelValue":o[0]||(o[0]=t=>l.pressedAlignLeft=t)},{default:n(()=>[e(d,{name:"align-left"})]),_:1},8,["modelValue"]),e(g,{modelValue:l.pressedAlignCenter,"onUpdate:modelValue":o[1]||(o[1]=t=>l.pressedAlignCenter=t)},{default:n(()=>[e(d,{name:"align-center"})]),_:1},8,["modelValue"]),e(g,{modelValue:l.pressedAlignRight,"onUpdate:modelValue":o[2]||(o[2]=t=>l.pressedAlignRight=t)},{default:n(()=>[e(d,{name:"align-right"})]),_:1},8,["modelValue"])]),_:1}),e(u,null,{default:n(()=>[e(g,{modelValue:l.pressedItalic,"onUpdate:modelValue":o[3]||(o[3]=t=>l.pressedItalic=t)},{default:n(()=>[e(d,{name:"italic"})]),_:1},8,["modelValue"]),e(g,{modelValue:l.pressedBold,"onUpdate:modelValue":o[4]||(o[4]=t=>l.pressedBold=t)},{default:n(()=>[e(d,{name:"bold"})]),_:1},8,["modelValue"]),e(g,{modelValue:l.pressedUnderline,"onUpdate:modelValue":o[5]||(o[5]=t=>l.pressedUnderline=t)},{default:n(()=>[e(d,{name:"link"})]),_:1},8,["modelValue"])]),_:1})]),_:1})]),_:1})),[[r,1]]),a(e(f,{code:l.code,lang:"html"},null,8,["code"]),[[r,1]]),e(V,{component:"LvToggleGroup"})],64)}const D=A(b,[["render",I]]);export{D as default};
---
+++ docs/lv-widget.2f5c8d3c.js
@@ -0,0 +1,25 @@
+import{_ as h,o,c as w,w as s,b as n,d as t,e,F as y,f as m,g as r,h as W}from"./index.33814177.js";const x=`<lv-widget title="Avg. Revenue" amount="1.239,21" prefix="$" :diff="-10" type="number" />
+<lv-widget title="Total Payout" amount="632,37" prefix="\u20AC" :diff="75" type="number" />`.trim(),D=`<lv-widget title="Followers" amount="2,530" :diff="10" type="chart" :labels="labels" :datasets="followersDatasets"/>
+<lv-widget title="Subscribers" chart-type="bar" amount="3,122" :diff="-2" type="chart" :labels="labels" :datasets="subscribersDatasets"/>`.trim(),C=`
+export default {
+    data() {
+        return {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            followersDatasets: [
+                {
+                    label: 'Followers',
+                    hue: 24,
+                    data: [55, 12, 20, 34, 7, 4],
+                },
+            ],
+            subscribersDatasets: [
+                {
+                    label: 'Subscribers',
+                    hue: 36,
+                    data: [20, 34, 6, 4,55, 12],
+                },
+            ],
+        }
+    }
+}
+`.trim(),F={data(){return{codeNumberWidget:x,codeChartWidget:D,codeChartWidgetScript:C,labels:["January","February","March","April","May","June"],followersDatasets:[{label:"Followers",hue:24,data:[55,12,20,34,7,4]}],subscribersDatasets:[{label:"Subscribers",hue:36,data:[20,34,6,4,55,12]}],activeTab:"number",tabs:[{id:"number",title:"Number Widget"},{id:"chart",title:"Chart Widget"}]}}},S=m("Widget"),T=m("Stats for nerds");function N(A,u,J,M,a,k){const b=r("lv-heading"),i=r("lv-widget",!0),c=r("lv-grid-column"),_=r("lv-grid-row"),f=r("lv-grid"),d=r("lv-code"),p=r("lv-tabs"),v=r("component-details"),l=W("space-after");return o(),w(y,null,[s((o(),n(b,{level:3},{default:t(()=>[S]),_:1})),[[l,.5]]),s((o(),n(b,{sub:"",level:6},{default:t(()=>[T]),_:1})),[[l,2]]),s((o(),n(p,{tabs:a.tabs,active:a.activeTab,onChangeTab:u[0]||(u[0]=g=>a.activeTab=g)},{number:t(()=>[s((o(),n(f,null,{default:t(()=>[e(_,{gap:"1rem"},{default:t(()=>[e(c,{width:6},{default:t(()=>[e(i,{title:"Avg. Revenue",amount:"1.239,21",prefix:"$",diff:-10,type:"number"})]),_:1}),e(c,{width:6},{default:t(()=>[e(i,{title:"Total Payout",amount:"632,37",prefix:"\u20AC",diff:75,type:"number"})]),_:1})]),_:1})]),_:1})),[[l,1]]),s(e(d,{lang:"html",code:a.codeNumberWidget},null,8,["code"]),[[l,1]])]),chart:t(()=>[s((o(),n(f,null,{default:t(()=>[e(_,{gap:"1rem"},{default:t(()=>[e(c,{width:6},{default:t(()=>[e(i,{title:"Followers",amount:"2,530",diff:10,type:"chart",labels:a.labels,datasets:a.followersDatasets},null,8,["labels","datasets"])]),_:1}),e(c,{width:6},{default:t(()=>[e(i,{title:"Subscribers","chart-type":"bar",amount:"3,122",diff:-2,type:"chart",labels:a.labels,datasets:a.subscribersDatasets},null,8,["labels","datasets"])]),_:1})]),_:1})]),_:1})),[[l,1]]),s(e(d,{lang:"html",code:a.codeChartWidget},null,8,["code"]),[[l,1]]),s(e(d,{lang:"javascript",code:a.codeChartWidgetScript},null,8,["code"]),[[l,1]])]),_:1},8,["tabs","active"])),[[l,1]]),e(v,{component:"LvWidget"})],64)}const P=h(F,[["render",N]]);export{P as default};
