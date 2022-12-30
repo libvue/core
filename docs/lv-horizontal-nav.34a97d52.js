754d884844498da31c10952554e2905c87f70b50
harmen.dev <hello@harmen.dev>
2022-12-30 22:29:28 +0100

    Add new docs

---
+++ docs/lv-horizontal-nav.34a97d52.js
@@ -0,0 +1,15 @@
+import{_ as h,o as r,c as f,w as t,b as v,d as o,e as l,F as b,f as _,g as a,h as z}from"./index.5a8dd38e.js";const g=`
+<lv-flex>
+    <lv-horizontal-nav-item v-space-after="1" label="Documentation">
+        <lv-horizontal-nav-group v-space-after="0.5" label="Getting Started">
+            <lv-horizontal-nav-item v-space-after="0.5" label="Install" />
+            <lv-horizontal-nav-item label="Uninstall" />
+        </lv-horizontal-nav-group>
+        <lv-horizontal-nav-group label="About us">
+            <lv-horizontal-nav-item v-space-after="0.5" label="Meet the team" />
+            <lv-horizontal-nav-item label="Jobs" />
+        </lv-horizontal-nav-group>
+    </lv-horizontal-nav-item>
+    <lv-horizontal-nav-item v-space-after="1" label="Extensions" />
+</lv-flex>
+`.trim(),x={data(){return{code:g}}},N=_("Horizontal Navigation"),H=_("Some components to help you construct a vertical Menu");function D(G,I,L,k,u,w){const c=a("lv-heading"),n=a("lv-horizontal-nav-item"),i=a("lv-horizontal-nav-group"),p=a("lv-flex"),m=a("lv-card"),d=a("lv-code"),s=a("component-details"),e=z("space-after");return r(),f(b,null,[t((r(),v(c,{level:3},{default:o(()=>[N]),_:1})),[[e,.5]]),t((r(),v(c,{sub:"",level:6},{default:o(()=>[H]),_:1})),[[e,2]]),t((r(),v(m,null,{default:o(()=>[l(p,null,{default:o(()=>[t((r(),v(n,{label:"Documentation"},{default:o(()=>[t((r(),v(i,{label:"Getting Started"},{default:o(()=>[t(l(n,{label:"Install"},null,512),[[e,.5]]),l(n,{label:"Uninstall"})]),_:1})),[[e,.5]]),l(i,{label:"About us"},{default:o(()=>[t(l(n,{label:"Meet the team"},null,512),[[e,.5]]),l(n,{label:"Jobs"})]),_:1})]),_:1})),[[e,1]]),t(l(n,{label:"Extensions"},null,512),[[e,1]])]),_:1})]),_:1})),[[e,1]]),t(l(d,{lang:"html",code:u.code},null,8,["code"]),[[e,1]]),t(l(s,{title:"LvHorizontalNavItem",component:"LvHorizontalNavItem"},null,512),[[e,1]]),l(s,{title:"LvHorizontalNavGroup",component:"LvHorizontalNavGroup"})],64)}const E=h(x,[["render",D]]);export{E as default};
