f6084bf35b249d934f1f93334ef8edd38cbebca1
Harmen de Vries <harmen@made.frl>
2023-01-03 14:34:47 +0100

    Add docs for 0.0.56

---
+++ docs/lv-button-group.04db3725.js
@@ -0,0 +1,7 @@
+import{_ as m,b as o,d as b,o as n,c as f,w as l,e as a,f as c,g as t,F as B,h as u}from"./index.139a13a1.js";const g=`
+<lv-button-group>
+    <lv-button label="Button One"/>
+    <lv-button label="Button Two"/>
+    <lv-button label="Button Three"/>
+</lv-button-group>
+`.trim(),h={data(){return{code:g}}},w=u("Button Group"),x=u("Used for making more condensed button groups");function T(k,y,G,$,_,C){const s=o("lv-heading"),r=o("lv-button"),d=o("lv-button-group",!0),p=o("lv-card"),i=o("lv-code"),v=o("component-details"),e=b("space-after");return n(),f(B,null,[l((n(),a(s,{level:3},{default:c(()=>[w]),_:1})),[[e,.5]]),l((n(),a(s,{sub:"",level:6},{default:c(()=>[x]),_:1})),[[e,2]]),l((n(),a(p,null,{default:c(()=>[t(d,null,{default:c(()=>[t(r,{label:"Button One",color:"solid-primary"}),t(r,{label:"Button Two",color:"solid-primary"}),t(r,{label:"Button Three",color:"solid-primary"})]),_:1})]),_:1})),[[e,1]]),l(t(i,{lang:"html",code:_.code},null,8,["code"]),[[e,1]]),t(v,{component:"LvButtonGroup"})],64)}const F=m(h,[["render",T]]);export{F as default};
---
+++ docs/lv-toggle.034dd26d.js
@@ -0,0 +1,8 @@
+import{_ as V,b as e,d as x,o as c,c as h,w as a,e as _,f as o,g as l,F as A,h as p}from"./index.139a13a1.js";const b=`
+<lv-toggle v-model="pressedItalic">
+    <lv-icon name="italic"/>
+</lv-toggle>
+<lv-toggle v-model="pressedAlign">
+    <lv-icon name="align-left"/>
+</lv-toggle>
+`.trim(),I={data(){return{code:b,pressedItalic:!1,pressedAlign:!1}}},T=p("Toggle"),k=p("A toggleable button");function w(B,n,C,D,t,F){const i=e("lv-heading"),r=e("lv-icon"),g=e("lv-toggle",!0),m=e("lv-flex"),v=e("lv-card"),u=e("lv-code"),f=e("component-details"),s=x("space-after");return c(),h(A,null,[a((c(),_(i,{level:3},{default:o(()=>[T]),_:1})),[[s,.5]]),a((c(),_(i,{sub:"",level:6},{default:o(()=>[k]),_:1})),[[s,2]]),a((c(),_(v,null,{default:o(()=>[l(m,null,{default:o(()=>[l(g,{modelValue:t.pressedItalic,"onUpdate:modelValue":n[0]||(n[0]=d=>t.pressedItalic=d)},{default:o(()=>[l(r,{name:"italic"})]),_:1},8,["modelValue"]),l(g,{modelValue:t.pressedAlign,"onUpdate:modelValue":n[1]||(n[1]=d=>t.pressedAlign=d)},{default:o(()=>[l(r,{name:"align-left"})]),_:1},8,["modelValue"])]),_:1})]),_:1})),[[s,1]]),a(l(u,{code:t.code,lang:"html"},null,8,["code"]),[[s,1]]),l(f,{component:"LvToggle"})],64)}const U=V(I,[["render",w]]);export{U as default};
