7701584026854e4bbf91602bc5e708a7d682ff43
Harmen de Vries <harmen@made.frl>
2023-01-12 09:34:53 +0100

    Add new docs

---
+++ docs/lv-blockquote-f76c0b37.js
@@ -0,0 +1,11 @@
+import{_ as v,b as o,d as f,o as t,c as h,w as a,e as n,f as c,g as s,F as g,h as l}from"./index-9aa14228.js";const k=`
+<lv-paragraph v-space-after="1">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+</lv-paragraph>
+<lv-blockquote v-space-after="1" caption="P. Rabbit">
+    This is a quote from someone I forgot the name of.
+</lv-blockquote>
+<lv-paragraph>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit
+</lv-paragraph>
+`.trim(),q={data(){return{codeBlockquote:k}}};function b(B,x,L,T,p,w){const r=o("lv-heading"),i=o("lv-paragraph"),u=o("lv-blockquote",!0),d=o("lv-card"),_=o("lv-code"),m=o("component-details"),e=f("space-after");return t(),h(g,null,[a((t(),n(r,{level:3},{default:c(()=>[l("Blockquote")]),_:1})),[[e,.5]]),a((t(),n(r,{level:6,sub:""},{default:c(()=>[l("The enclosed text is an extended quotation")]),_:1})),[[e,1]]),a((t(),n(d,null,{default:c(()=>[a((t(),n(i,null,{default:c(()=>[l(" Lorem ipsum dolor sit amet, consectetur adipiscing elit ")]),_:1})),[[e,1]]),a((t(),n(u,{caption:"P. Rabbit"},{default:c(()=>[l(" This is a quote from someone I forgot the name of. ")]),_:1})),[[e,1]]),s(i,null,{default:c(()=>[l(" Lorem ipsum dolor sit amet, consectetur adipiscing elit ")]),_:1})]),_:1})),[[e,1]]),a(s(_,{lang:"html",code:p.codeBlockquote},null,8,["code"]),[[e,1]]),s(m,{component:"LvBlockquote"})],64)}const C=v(q,[["render",b]]);export{C as default};
--- docs/lv-progress-bar-d3ec9ad3.js
+++
@@ -1,3 +0,0 @@
-import{_ as m,b as e,d as u,o as t,c as f,w as s,e as r,f as a,g as c,F as g,h as l}from"./index-7fe7c56b.js";const h=`
-<lv-progress-bar :value="60"/>
-`.trim(),B={data(){return{code:h}}};function b(x,k,w,P,_,$){const n=e("lv-heading"),d=e("lv-progress-bar",!0),p=e("lv-card"),v=e("lv-code"),i=e("component-details"),o=u("space-after");return t(),f(g,null,[s((t(),r(n,{level:3},{default:a(()=>[l("Progress Bar")]),_:1})),[[o,.5]]),s((t(),r(n,{sub:"",level:6},{default:a(()=>[l("Used for showing the completion progress of a task.")]),_:1})),[[o,2]]),s((t(),r(p,null,{default:a(()=>[c(d,{value:60})]),_:1})),[[o,1]]),s(c(v,{code:_.code,lang:"html"},null,8,["code"]),[[o,1]]),c(i,{component:"LvProgressBar"})],64)}const D=m(B,[["render",b]]);export{D as default};
