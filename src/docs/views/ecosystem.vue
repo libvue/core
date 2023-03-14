dc495b2512d93225b66c40ce4c2aaae5b612d1e3
harmen.dev <hello@harmen.dev>
2023-03-14 20:34:45 +0100

    change some lang

--- src/docs/views/ecosystem.vue
+++ src/docs/views/ecosystem.vue
@@ -6,6 +6,6 @@
     <lv-paragraph v-space-after="1">
         A Laravel Orion compatible repository-based Javascript http-client powered by axios.
     </lv-paragraph>
-    <lv-code v-space-after="2" code="npm install --save @libvue/laravel-orion-api" lang="markup"/>
+    <lv-code v-space-after="2" code="npm install --save @libvue/laravel-orion-api" lang="bash"/>
 
 </template>
