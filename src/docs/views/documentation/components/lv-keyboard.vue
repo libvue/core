bbad30261103082d3b0b07366f3cbf9a250357f8
Harmen de Vries <harmen@made.frl>
2023-11-10 16:22:26 +0100

    Fix code preview

--- src/docs/views/documentation/components/lv-keyboard.vue
+++ src/docs/views/documentation/components/lv-keyboard.vue
@@ -28,8 +28,21 @@
 import LvText from "../../../../lib/components/LvText/LvText.vue";
 
 const code = `
-Press <lv-keyboard meta>D</lv-keyboard> to minimize all windows.
-Press <lv-keyboard ctrl alt shift>DEL</lv-keyboard> to open the task manager.
+<lv-heading v-space-after="0.5" :level="6">Mac layout</lv-heading>
+<lv-text v-space-after="1">
+    Press <lv-keyboard meta>D</lv-keyboard> to minimize all windows.
+    Press <lv-keyboard ctrl alt shift>DEL</lv-keyboard> to open the task manager.
+</lv-text>
+<lv-heading v-space-after="0.5" :level="6">Windows layout</lv-heading>
+<lv-text v-space-after="1">
+    Press <lv-keyboard meta layout="windows">D</lv-keyboard> to minimize all windows.
+    Press <lv-keyboard ctrl alt shift layout="windows">DEL</lv-keyboard> to open the task manager.
+</lv-text>
+<lv-heading v-space-after="0.5" :level="6">Linux layout</lv-heading>
+<lv-text>
+    Press <lv-keyboard meta layout="linux">D</lv-keyboard> to minimize all windows.
+    Press <lv-keyboard ctrl alt shift layout="linux">DEL</lv-keyboard> to open the task manager.
+</lv-text>
 `.trim();
 
 export default {
