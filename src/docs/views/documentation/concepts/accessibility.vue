f9e4e14ea26a702ce93c9bd19f37e54842acf991
Harmen de Vries <harmen@made.frl>
2023-11-15 15:50:56 +0100

    Improve accessibility docs

--- src/docs/views/documentation/concepts/accessibility.vue
+++ src/docs/views/documentation/concepts/accessibility.vue
@@ -13,19 +13,26 @@
         </lv-flex>
     </lv-notice>
 
+    <lv-heading v-space-after="1" :level="5">How to use</lv-heading>
     <lv-text v-space-after="1">
-        Below is a list of design choices we made or you should be aware of.
+        Our components set some attributes like <lv-code>role</lv-code>, <lv-code>aria-modal</lv-code> or <lv-code>aria-checked</lv-code> automatically, but attributes
+        like <lv-code>aria-label</lv-code> or <lv-code>aria-labelledBy</lv-code> are up to you to add when applicable. You can see on the details page of a component
+        which aria props are available. Tip: Use lighthouse to check if you're not missing anything.
     </lv-text>
 
     <lv-heading v-space-after="1" :level="5">Design Choices</lv-heading>
     <lv-list v-space-after="1">
+        <lv-list-item>
+            No ARIA is better then bad ARIA, but because we've added some aria attributes automatically, you MUST add the missing aria attributes yourself. In the future
+            we might consider making these accesibility attributes optional.
+        </lv-list-item>
         <lv-list-item>
             All form elements except radio buttons in libvue are still focusable when they are disabled.
         </lv-list-item>
         <lv-list-item>
             Our form elements support placeholders but you should always use labels for form elements and placeholders as an addition.
             <lv-list>
-                <lv-list-item>LvCheckbox, LvSwitch and the LvRadio component have a prop:label and prop:labelFor that inserts a label as text next to it.</lv-list-item>
+                <lv-list-item>LvCheckbox, LvSwitch and the LvRadio component have a prop:label and prop:labelFor that inserts a label next to it.</lv-list-item>
                 <lv-list-item>All other form elements should use LvText with the prop:as set to 'label' and the prop:labelFor set to the id of the form element.</lv-list-item>
             </lv-list>
         </lv-list-item>
