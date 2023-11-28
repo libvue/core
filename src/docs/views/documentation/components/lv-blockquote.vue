bfc806520a2a8a58347a7c08f80c6cacae4e062f
harmendv <harmendv@gmail.com>
2023-11-28 22:01:07 +0100

    Improve blockquote

--- src/docs/views/documentation/components/lv-blockquote.vue
+++ src/docs/views/documentation/components/lv-blockquote.vue
@@ -2,11 +2,7 @@
     <lv-heading v-space-after="0.5" :level="3">Blockquote</lv-heading>
     <lv-heading v-space-after="1" :level="6" sub>The enclosed text is an extended quotation</lv-heading>
     <lv-card v-space-after="1">
-        <lv-text v-space-after="1"> Lorem ipsum dolor sit amet, consectetur adipiscing elit </lv-text>
-        <lv-blockquote v-space-after="1" caption="P. Rabbit">
-            This is a quote from someone I forgot the name of.
-        </lv-blockquote>
-        <lv-text> Lorem ipsum dolor sit amet, consectetur adipiscing elit </lv-text>
+        <lv-blockquote caption="P. Rabbit">This is a quote from someone I forgot the name of.</lv-blockquote>
     </lv-card>
 
     <code-preview v-space-after="1" lang="html" :code="codeBlockquote" />
