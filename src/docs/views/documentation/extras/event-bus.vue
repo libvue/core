4e754c7a6811227e5bd93db72427d4243b0919b1
harmendv <harmendv@gmail.com>
2023-12-18 09:12:54 +0100

    Improve eventbus docs a bit

--- src/docs/views/documentation/extras/event-bus.vue
+++ src/docs/views/documentation/extras/event-bus.vue
@@ -1,14 +1,29 @@
 <template>
     <lv-heading v-space-after="0.5" :level="3">eventBus</lv-heading>
     <lv-heading v-space-after="2" sub :level="6">A simple eventBus using tiny-emitter</lv-heading>
-    <lv-heading v-space-after="1" :level="6">Install eventBus utility</lv-heading>
-    <code-preview v-space-after="1" :code="installDirective" lang="javascript" />
+    <lv-heading v-space-after="1" :level="6">Example</lv-heading>
+    <code-preview v-space-after="1" :code="usageCode" lang="javascript" />
 </template>
 
 <script>
+const usageCode = `
+import { eventBus } from '@libvue/core';
+
+export default {
+   mounted() {
+      this.eventBus.$on('eventName', (msg) => { console.log(msg) });
+   },
+   methods: {
+      onClick() {
+         this.eventBus.$emit('eventName', 'hello world');
+      }
+   }
+}
+`.trim();
 export default {
     data() {
         return {
+            usageCode,
             installDirective: `import { eventBus } from '@libvue/core';`,
         };
     },
